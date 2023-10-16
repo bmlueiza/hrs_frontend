31c6f990a164e633a882e396a5e5fe560a6953eb
Belén <belen.munoz@usach.cl>
2023-10-15 23:44:26 -0300

    feat: vista protipo pacientes
    Tabla responsiva con las características generales de los pacientes.

---
+++ pages/pacientes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="table-responsive">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th class="header" scope="col">#</th>
+            <th class="header" scope="col">Nombres</th>
+            <th class="header" scope="col">1° Apellido</th>
+            <th class="header" scope="col">2° Apellido</th>
+            <th class="header" scope="col">RUT</th>
+            <th class="header" scope="col">Teléfono</th>
+            <th class="header" scope="col">Dirección</th>
+            <th class="header" scope="col">Riesgo</th>
+            <th class="header" scope="col">Diagnóstico</th>
+            <th class="header" scope="col">Dirección</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.header {
+  position: sticky;
+  top: 0;
+}
+</style>

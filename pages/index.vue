8bc16fce48af10eb194eabbcdb300e35d70f7cd2
Belén <belen.munoz@usach.cl>
2023-10-25 16:25:01 -0300

    style: change vista login

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="gradient-custom">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="container">
+      <div class="row d-flex justify-content-center align-items-center">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card bg-dark text-white" style="border-radius: 1rem">
+          <div class="card">
             <div class="card-body p-5 text-center">
               <div class="mb-md-5 mt-md-4 pb-5">
                 <h2 class="fw-bold mb-2 text-uppercase">HRS</h2>
-                <p class="text-white-50 mb-5">
+                <p class="mb-5">
                   Por favor, ingresa tu correo y tu contraseña.
                 </p>
 
@@ -16,6 +16,7 @@
                     type="email"
                     id="typeEmailX"
                     class="form-control form-control-lg"
+                    placeholder="Ingrese su correo electrónico"
                   />
                   <label class="form-label" for="typeEmailX">Correo</label>
                 </div>
@@ -25,6 +26,7 @@
                     type="password"
                     id="typePasswordX"
                     class="form-control form-control-lg"
+                    placeholder="Ingrese su contraseña"
                   />
                   <label class="form-label" for="typePasswordX"
                     >Contraseña</label
@@ -32,28 +34,16 @@
                 </div>
 
                 <p class="small mb-5 pb-lg-2">
-                  <a class="text-white-50" href="#!"
-                    >¿Olvidaste tu contraseña?</a
-                  >
+                  <a class="text" href="#!">¿Olvidaste tu contraseña?</a>
                 </p>
 
-                <button class="btn btn-outline-light btn-lg px-5" type="submit">
-                  <a href="/recomendaciones">Iniciar sesión</a>
-                </button>
-
-                <div
-                  class="d-flex justify-content-center text-center mt-4 pt-1"
+                <button
+                  onclick="location.href='/recomendaciones'"
+                  class="btn btn-primary btn-lg px-5"
+                  type="submit"
                 >
-                  <a href="#!" class="text-white"
-                    ><i class="fab fa-facebook-f fa-lg"></i
-                  ></a>
-                  <a href="#!" class="text-white"
-                    ><i class="fab fa-twitter fa-lg mx-4 px-2"></i
-                  ></a>
-                  <a href="#!" class="text-white"
-                    ><i class="fab fa-google fa-lg"></i
-                  ></a>
-                </div>
+                  Iniciar sesión
+                </button>
               </div>
             </div>
           </div>
@@ -69,4 +59,15 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin-top: 10px;
+  max-height: 100vh;
+}
+
+.card {
+  background-color: #dbe2ef;
+  color: #000;
+  border-radius: 1rem;
+}
+</style>

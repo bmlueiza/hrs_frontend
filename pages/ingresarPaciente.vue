f3cd2094f902125068a83ca796b7982b98bb98d4
Belén <belen.munoz@usach.cl>
2023-10-30 03:47:46 -0300

    refactor: change selección diagnósticos pacientes con multiselect

--- pages/ingresarPaciente.vue
+++ pages/ingresarPaciente.vue
@@ -2,64 +2,87 @@
   <div>
     <Navbar />
     <div class="container">
+      <div class="form-title">
+        <h1>Ingresar paciente</h1>
+      </div>
       <form>
         <!-- 2 column grid layout with text inputs for the first and last names -->
         <div class="row mb-4">
+          <!-- Columna Nombres-->
           <div class="col">
             <div class="form-outline">
               <input type="text" id="form6Example1" class="form-control" />
               <label class="form-label" for="form6Example1">Nombres</label>
             </div>
           </div>
+          <!-- Columna Primer apellido-->
           <div class="col">
             <div class="form-outline">
               <input type="text" id="form6Example2" class="form-control" />
               <label class="form-label" for="form6Example2"
-                >Apellido paterno</label
+                >Primer apellido</label
               >
             </div>
           </div>
+          <!-- Columna Segundo apellido-->
           <div class="col">
             <div class="form-outline">
               <input type="text" id="form6Example3" class="form-control" />
               <label class="form-label" for="form6Example3"
-                >Apellido materno</label
+                >Segundo apellido</label
               >
             </div>
           </div>
         </div>
+        <div class="row mb-4">
+          <!-- Input RUT -->
+          <div class="col">
+            <div class="form-outline">
+              <input type="text" id="form6Example9" class="form-control" />
+              <label class="form-label" for="form6Example9">RUT</label>
+            </div>
+          </div>
 
-        <!-- Text input -->
-        <div class="form-outline mb-4">
-          <input type="text" id="form6Example9" class="form-control" />
-          <label class="form-label" for="form6Example9">RUT</label>
+          <!-- Input Teléfono -->
+          <div class="col">
+            <div class="form-outline">
+              <input type="text" id="form6Example4" class="form-control" />
+              <label class="form-label" for="form6Example4">Teléfono</label>
+            </div>
+          </div>
+
+          <!-- Input Dirección -->
+          <div class="col">
+            <div class="form-outline">
+              <input type="email" id="form6Example5" class="form-control" />
+              <label class="form-label" for="form6Example5">Dirección</label>
+            </div>
+          </div>
         </div>
 
-        <!-- Text input -->
+        <!-- Input Diagnóstico -->
         <div class="form-outline mb-4">
-          <input type="text" id="form6Example4" class="form-control" />
-          <label class="form-label" for="form6Example4">Teléfono</label>
+          <multiselect
+            v-model="selectedDiagnosticos"
+            :options="diagnosticos"
+            filter
+            track-by="id"
+            label="codigo"
+            placeholder="Seleccione los diagnósticos de este paciente"
+            :maxSelected="4"
+            display="chip"
+            class="w-full md:w-20rem"
+          >
+          </multiselect>
         </div>
 
-        <!-- Email input -->
-        <div class="form-outline mb-4">
-          <input type="email" id="form6Example5" class="form-control" />
-          <label class="form-label" for="form6Example5">Dirección</label>
-        </div>
-
-        <!-- Number input -->
-        <div class="form-outline mb-4">
-          <input type="number" id="form6Example6" class="form-control" />
-          <label class="form-label" for="form6Example6">Diagnóstico</label>
-        </div>
-
-        <!-- Message input -->
-        <div class="form-outline mb-4">
+        <!-- Input Observaciones-->
+        <div class="form-outline mb-2">
           <textarea class="form-control" id="form6Example7" rows="4"></textarea>
           <label class="form-label" for="form6Example7">Observaciones</label>
         </div>
 
-        <!-- Submit button -->
+        <!-- Botón Ingresar paciente -->
         <button type="submit" class="btn btn-primary btn-block mb-10">
           Ingresar paciente
         </button>
@@ -68,14 +91,54 @@
   </div>
 </template>
 
+<script>
+import axios from 'axios'
+import Multiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    Multiselect,
+  },
+  data() {
+    return {
+      diagnosticos: [],
+      selectedDiagnostico: [],
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:8000/hrsapp/api/diagnosticos/')
+      .then((response) => {
+        console.log(response)
+        this.diagnosticos = response.data
+      })
+      .catch((error) => {
+        console.log('Error al obtener los datos de diagnosticos:', error)
+      })
+  },
+}
+</script>
+
 <style>
+.container {
+  justify-content: center;
+  margin-top: 2%;
+  margin-bottom: 2%;
+  color: aliceblue;
+  border-color: black;
+  border-radius: 5%;
+  background-color: #112d4e;
+}
 .form {
-  margin: 20px;
-  margin-top: 10px;
+  color: aliceblue;
+  align-self: center;
+}
+.form-title {
+  text-align: center;
   color: aliceblue;
 }
-.container {
-  padding-top: 10px;
-  color: aliceblue;
+
+.btn {
+  margin: 2%;
 }
 </style>

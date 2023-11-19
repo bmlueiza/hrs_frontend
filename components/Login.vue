7f467d8ec6e666b9b3c033ab25b65c99f1e555ce
Belén <belen.munoz@usach.cl>
2023-11-18 21:08:34 -0300

    refactor: login como componente
    - Se puso un login como componente Login.vue para ser utilizado en index.vue.
    - Se cambió el estilo del login para hacer una vista sencilla pero más agradable. Siguiendo el diseño responsivo.
    - Se deja predipuesto a aplicar la lógica de inicio de sesión.

---
+++ components/Login.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h1 class="text-center">HRS</h1>
+      <h3 class="text-center">Iniciar Sesión</h3>
+    </div>
+    <div class="card-body">
+      <form @submit.prevent="login">
+        <div class="mb-3">
+          <label for="email" class="form-label">Correo</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            autocomplete="off"
+            v-model="email"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Contraseña</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            autocomplete="off"
+            v-model="password"
+            required
+          />
+        </div>
+        <div class="button text-center">
+          <button type="submit" class="btn">Iniciar Sesión</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    login() {
+      // Aquí puedes agregar la lógica para enviar la solicitud de inicio de sesión al servidor
+      // Por ahora, simplemente imprimo los datos en la consola
+      console.log('Usuario:', this.email)
+      console.log('Contraseña:', this.password)
+
+      // Redireccionar a la página de recomendaciones si el inicio de sesión es exitoso
+      this.$router.push('/recomendaciones')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.card {
+  width: 50%;
+  margin: 0 auto;
+  margin-top: 10%;
+  border: 1px solid #112d4e;
+  border-radius: 10px;
+  box-shadow: 0 0 10px #000000;
+  background-color: #112d4e;
+}
+.card-header {
+  background-color: #112d4e;
+  color: white;
+}
+.card-body {
+  background-color: white;
+}
+.form-control {
+  align-self: center;
+  border: 1px solid #112d4e;
+  border-radius: 8px;
+}
+.btn {
+  background-color: #112d4e;
+  color: white;
+}
+
+@media (max-width: 576px) {
+  .card {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,16 @@
 <template>
-  <section class="gradient-custom">
-    <div class="container">
-      <div class="row d-flex justify-content-center align-items-center">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card">
-            <div class="card-body p-5 text-center">
-              <div class="mb-md-5 mt-md-4 pb-5">
-                <h2 class="fw-bold mb-2 text-uppercase">HRS</h2>
-                <p class="mb-5">
-                  Por favor, ingresa tu correo y tu contraseña.
-                </p>
-
-                <div class="form-outline form-white mb-4">
-                  <input
-                    type="email"
-                    id="typeEmailX"
-                    class="form-control form-control-lg"
-                    placeholder="Ingrese su correo electrónico"
-                  />
-                  <label class="form-label" for="typeEmailX">Correo</label>
-                </div>
-
-                <div class="form-outline form-white mb-4">
-                  <input
-                    type="password"
-                    id="typePasswordX"
-                    class="form-control form-control-lg"
-                    placeholder="Ingrese su contraseña"
-                  />
-                  <label class="form-label" for="typePasswordX"
-                    >Contraseña</label
-                  >
-                </div>
-
-                <p class="small mb-5 pb-lg-2">
-                  <a class="text" href="#!">¿Olvidaste tu contraseña?</a>
-                </p>
-
-                <button
-                  onclick="location.href='/recomendaciones'"
-                  class="btn btn-primary btn-lg px-5"
-                  type="submit"
-                >
-                  Iniciar sesión
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <div class="gradient-custom">
+    <Login />
+  </div>
 </template>
 
 <script>
+import Login from '../components/Login.vue'
+
 export default {
   name: 'IndexPage',
+  components: { Login },
 }
 </script>
 
-<style scoped>
-.container {
-  margin-top: 10px;
-  max-height: 100vh;
-}
-
-.card {
-  background-color: #dbe2ef;
-  color: #000;
-  border-radius: 1rem;
-}
-</style>
+<style></style>

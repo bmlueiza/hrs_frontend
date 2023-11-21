dca373cfcf923bed74c843376f323261a8469d7f
Belén <belen.munoz@usach.cl>
2023-11-21 18:24:13 -0300

    refactor: add componente TablaMedicos
    - Se creó el componente TablaMedicos.vue pensando en la escalabilidad de la aplicación y la facilidad para edición de estilo.
    - Se cambió el formato y estilo de la vista de medicos.vue al nuevo estilo impuesto desde el commit "style: nuevo formato vista gestores".

--- pages/medicos.vue
+++ pages/medicos.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="médicos">
     <Navbar />
-    <div class="table-responsive">
-      <table class="table table-light table-striped table-bordered">
-        <!--Cabecera de la tabla Médicos-->
-        <thead>
-          <tr>
-            <th class="header" scope="col">#</th>
-            <th class="header" scope="col">RUT</th>
-            <th class="header" scope="col">Nombre</th>
-            <th class="header" scope="col">Apellido</th>
-            <th class="header" scope="col">Especialidad</th>
-          </tr>
-        </thead>
-        <!--Contenido de la tabla Médicos-->
-        <tbody>
-          <tr v-for="medico in todosLosMedicos" :key="medico.id">
-            <th scope="row">{{ medico.id }}</th>
-            <td>{{ medico.rut }}</td>
-            <td>{{ medico.nombre }}</td>
-            <td>{{ medico.apellido }}</td>
-            <td>{{ medico.especialidad }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="container">
+      <section class="content-header">
+        <div class="row">
+          <div class="col-xs-12 col-md-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Buscar por nombre"
+              id="buscarMédico"
+            />
+          </div>
+          <div class="col-xs-12 col-md-9 d-flex justify-content-end">
+            <div class="btn-group pull-right">
+              <button type="button" class="btn btn-secondary">
+                Añadir médico
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content-header -->
+      <!-- Listado de Médicos-->
+      <section class="content">
+        <!--Primera fila-->
+        <div class="row">
+          <!--Primera columna-->
+          <div class="col-md-12">
+            <div class="box">
+              <div class="box-header with-border">
+                <h4 class="box-title">Listado de Médicos</h4>
+              </div>
+              <div class="box-body">
+                <TablaMedicos />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content -->
     </div>
+    <!-- /.container -->
   </div>
 </template>
 <script>
-import axios from 'axios'
+import Navbar from '@/components/Navbar.vue'
+import TablaMedicos from '@/components/tablas/TablaMedicos.vue'
 
 export default {
-  data() {
-    return {
-      todosLosMedicos: [],
-    }
-  },
-  mounted() {
-    axios
-      .get('http://localhost:8000/hrsapp/api/medicos')
-      .then((response) => {
-        this.todosLosMedicos = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+  components: {
+    Navbar,
+    TablaMedicos,
   },
 }
 </script>

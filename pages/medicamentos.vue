dfcf624728a1c75176b3a93e5f414fba33a42039
Belén <belen.munoz@usach.cl>
2023-11-29 00:22:56 -0300

    feat: vista completa Medicamentos
    - Permite ver el listado de medicamentos.
    - Integra opción de búsqueda.
    - Permite buscar por rut, nombre o código.
    - Abre formulario para ingresar nuevo médicamento (FormCrearMedicamento.vue) en vista modal.

--- pages/medicamentos.vue
+++ pages/medicamentos.vue
@@ -4,20 +4,52 @@
     <div class="container">
       <section class="content-header">
         <div class="row">
-          <div class="col-xs-12 col-md-3">
+          <div class="col-lg-6 col-md-12 d-flex">
             <input
               type="text"
               class="form-control"
-              placeholder="Buscar por nombre"
+              placeholder="Buscar por nombre o código"
               id="buscarMedicamento"
+              v-model="terminoBusqueda"
+              @input="manejarInput"
             />
+            <button
+              @click="buscarMedicamentos"
+              class="btn btn-primary"
+              type="button"
+              :disabled="terminoBusqueda.trim() === ''"
+            >
+              Buscar
+            </button>
+            <button
+              @click="limpiarBusqueda"
+              class="btn btn-primary"
+              type="button"
+              :disabled="terminoBusqueda.trim() === ''"
+            >
+              Cancelar
+            </button>
           </div>
-          <div class="col-xs-12 col-md-9 d-flex justify-content-end">
+          <div
+            class="col-lg-6 col-md-12 d-flex justify-content-lg-end justify-content-md-start mt-3 mt-md-0"
+          >
             <div class="btn-group pull-right">
-              <button type="button" class="btn btn-secondary">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-toggle="modal"
+                :data-bs-target="`#${modalId}`"
+              >
                 Añadir medicamento
               </button>
             </div>
+            <Modal
+              ref="modal"
+              :modalId="modalId"
+              :modalTitle="modalTitle"
+              :componenteFormulario="componenteFormulario"
+              :mostrarModal="mostrarModal"
+            />
           </div>
         </div>
       </section>
@@ -31,7 +63,11 @@
                 <h4 class="box-title">Listado de Medicamentos</h4>
               </div>
               <div class="box-body">
-                <TablaMedicamentos />
+                <TablaMedicamentos
+                  ref="tablaMedicamentos"
+                  @actualizarTabla="actualizarTabla"
+                  :terminoBusqueda="terminoBusqueda"
+                />
               </div>
             </div>
           </div>
@@ -44,12 +80,57 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Modal from '@/components/modales/Modal.vue'
 import TablaMedicamentos from '@/components/tablas/TablaMedicamentos.vue'
+import FormCrearMedicamento from '@/components/formularios/crear/FormCrearMedicamento.vue'
 
 export default {
   components: {
     Navbar,
+    Modal,
     TablaMedicamentos,
+    FormCrearMedicamento,
+  },
+  data() {
+    return {
+      terminoBusqueda: '',
+      mostrarModal: false,
+      modalId: 'modalId',
+      modalTitle: 'Añadir nuevo medicamento',
+      componenteFormulario: FormCrearMedicamento,
+    }
+  },
+  methods: {
+    async buscarMedicamentos() {
+      this.$nextTick(() => {
+        this.$refs.tablaMedicamentos.actualizarTabla()
+      })
+    },
+    actualizarTabla() {
+      this.tablaActualizada = true
+      this.$nextTick(() => {
+        this.tablaActualizada = false
+      })
+    },
+    async limpiarBusqueda() {
+      this.terminoBusqueda = ''
+      this.$nextTick(() => {
+        this.$refs.tablaMedicamentos.actualizarTabla()
+      })
+    },
+    async manejarInput() {
+      if (this.terminoBusqueda.trim() === '') {
+        this.$nextTick(() => {
+          this.$refs.tablaMedicamentos.actualizarTabla()
+        })
+      }
+    },
   },
 }
 </script>
+<style scoped>
+.form-control {
+  height: fit-content !important;
+  align-self: center;
+}
+</style>

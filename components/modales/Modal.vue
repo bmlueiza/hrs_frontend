7f3deca63d309594bb2e7669c395ed159a8dacb6
Belén <belen.munoz@usach.cl>
2023-11-29 00:20:25 -0300

    feat: vista modal
    - Estructura de vista modal, pensada para usar con formularios pequeños y posibles textos a mostrar al paciente.

---
+++ components/modales/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="modalId"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      data-backdrop="static"
+      data-keyboard="false"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{ modalTitle }}</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <component :is="componenteFormulario" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modalId: {
+      type: String,
+      required: true,
+    },
+    modalTitle: {
+      type: String,
+      required: true,
+    },
+    componenteFormulario: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>

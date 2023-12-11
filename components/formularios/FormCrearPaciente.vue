0dd80043555d995cc6028880945cb074963735f7
Belén <belen.munoz@usach.cl>
2023-12-10 22:23:48 -0300

    refactor: change para trabajar con los CHOICES del modelo paciente
    - Además, cambia las validaciones al formato Bootstrap en vez de depender del navegador.

--- components/formularios/FormCrearPaciente.vue
+++ components/formularios/FormCrearPaciente.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="FormCrearPaciente">
-    <form>
+    <!-- Aviso de paciente agregado correctamente-->
+    <div class="avisos text-center">
+      <div v-if="mensajeAviso" class="alert alert-success" role="alert">
+        {{ mensajeAviso }}
+      </div>
+      <!-- Aviso de error al agregar paciente -->
+      <div v-if="mensajeError" class="alert alert-danger" role="alert">
+        {{ mensajeError }}
+      </div>
+    </div>
+    <form class="crear-paciente">
       <!-- Primera fila -->
       <div class="row mb-4">
         <!-- Primera columna - nombres-->
@@ -81,20 +91,19 @@
         <div class="col">
           <div class="form-outline">
             <label class="form-label" for="sexo">Sexo</label>
-            <multiselect
+            <select
               id="sexo"
-              v-model="sexoSeleccionado"
-              :options="sexos"
-              :multiple="false"
-              :searchable="false"
-              placeholder="Sexo del paciente"
-              :select-label="'Seleccionar'"
-              :deselect-label="'Eliminar selección'"
-              :selected-label="'Seleccionado'"
-              :close-on-select="true"
-              display="chip"
+              v-model="nuevoPaciente.sexo"
+              class="form-select"
               required
-            ></multiselect>
+            >
+              <option value="" disabled selected hidden>
+                Sexo del paciente
+              </option>
+              <option v-for="sexo in sexos" :key="sexo[0]" :value="sexo[0]">
+                {{ sexo[1] }}
+              </option>
+            </select>
           </div>
         </div>
 
@@ -119,20 +128,23 @@
         <div class="col">
           <div class="form-outline">
             <label class="form-label" for="riesgo">Riesgo</label>
-            <multiselect
+            <select
               id="riesgo"
-              v-model="riesgoSeleccionado"
-              :options="riesgos"
-              :multiple="false"
-              :searchable="false"
-              placeholder="Riesgo del paciente"
-              :select-label="'Seleccionar'"
-              :deselect-label="'Eliminar selección'"
-              :selected-label="'Seleccionado'"
-              :close-on-select="true"
-              display="chip"
+              v-model="nuevoPaciente.riesgo"
+              class="form-select"
               required
-            ></multiselect>
+            >
+              <option value="" disabled selected hidden>
+                Riesgo del paciente
+              </option>
+              <option
+                v-for="riesgo in riesgos"
+                :key="riesgo[0]"
+                :value="riesgo[0]"
+              >
+                {{ riesgo[1] }}
+              </option>
+            </select>
           </div>
         </div>
       </div>
@@ -193,7 +205,6 @@
               :selected-label="'Seleccionado'"
               :no-result="'No se encontraron diagnósticos'"
               :no-options="'No se encontraron diagnósticos'"
-              :maxSelected="4"
               :close-on-select="false"
               :preselect-first="true"
               display="chip"
@@ -206,14 +217,10 @@
 
       <!-- Botón Ingresar paciente -->
       <div class="text-center">
-        <button
-          type="submit"
-          class="btn btn-primary"
-          @click.prevent="crearPaciente"
-        >
+        <button type="submit" class="btn btn-primary" @submit="crearPaciente">
           Ingresar paciente
         </button>
-        <button type="reset" class="btn btn-primary" @click.prevent="cancelar">
+        <button type="reset" class="btn btn-primary" @click="limpiarFormulario">
           Cancelar
         </button>
       </div>
@@ -232,8 +239,8 @@ export default {
   data() {
     return {
       listaDiagnosticos: [],
-      sexos: ['No especificado', 'Masculino', 'Femenino', 'No aplicable'],
-      riesgos: ['Bajo', 'Medio', 'Alto'],
+      sexos: [],
+      riesgos: [],
       sexoSeleccionado: '',
       riesgoSeleccionado: '',
       nuevoPaciente: {
@@ -242,31 +249,50 @@ export default {
         apellido1: '',
         apellido2: '',
         fecha_nacimiento: null,
-        sexo: 0,
+        sexo: '',
         telefono: '',
         direccion: '',
-        riesgo: 0,
+        riesgo: '',
         diagnosticos: [],
         gestor: 1,
       },
+      // Avisos
+      mensajeError: '',
+      mensajeAviso: '',
     }
   },
   mounted() {
     axios
       .get(this.$axios.defaults.baseURL + 'diagnosticos/')
       .then((response) => {
-        console.log(response)
         this.listaDiagnosticos = response.data
+        console.log('Diagnosticos:', this.listaDiagnosticos)
       })
       .catch((error) => {
         console.log('Error al obtener los datos de diagnosticos:', error)
-      })
+      }),
+      axios
+        .get(this.$axios.defaults.baseURL + 'pacientes/riesgos')
+        .then((response) => {
+          this.riesgos = response.data
+          console.log('Riesgos:', this.riesgos)
+        })
+        .catch((error) => {
+          console.log('Error al obtener los datos de riesgos:', error)
+        }),
+      axios
+        .get(this.$axios.defaults.baseURL + 'pacientes/sexos')
+        .then((response) => {
+          this.sexos = response.data
+          console.log('Sexos:', this.sexos)
+        })
+        .catch((error) => {
+          console.log('Error al obtener los datos de sexos:', error)
+        })
   },
   methods: {
     async crearPaciente() {
       if (this.validarFormulario()) {
-        this.nuevoPaciente.sexo = this.mapSexoANum(this.sexoSeleccionado)
-        this.nuevoPaciente.riesgo = this.mapRiesgoANum(this.riesgoSeleccionado)
         this.nuevoPaciente.diagnosticos = this.nuevoPaciente.diagnosticos.map(
           (diagnostico) => diagnostico.id
         )
@@ -283,122 +309,52 @@ export default {
           )
           .then((response) => {
             console.log(response)
-            alert('Paciente creado exitosamente')
-            this.form = {
-              sexoSeleccionado: '',
-              riesgoSeleccionado: '',
-              nuevoPaciente: {
-                rut: '',
-                nombres: '',
-                apellido1: '',
-                apellido2: '',
-                fecha_nacimiento: null,
-                sexo: 0,
-                telefono: '',
-                direccion: '',
-                riesgo: 0,
-                diagnosticos: [],
-                gestor: 1,
-              },
-            }
+            this.limpiarFormulario()
+            this.mensajeAviso = 'Paciente creado exitosamente'
           })
           .catch((error) => {
             console.log('Error:', error.response.data)
-            alert('Error al crear el paciente')
+            this.limpiarFormulario()
+            this.mensajeError = 'Error al crear el paciente'
           })
       }
     },
-    async cancelar() {
-      const confirmacion = window.confirm(
-        '¿Estás seguro que deseas cancelar el ingreso de este paciente?'
-      )
-      if (confirmacion) {
-        this.form = {
-          nombres: '',
-          primer_apellido: '',
-          segundo_apellido: '',
-          rut: '',
-          sexo: '',
-          fechaNacimiento: '',
-          telefono: '',
-          direccion: '',
-          alergias: '',
-          selectedDiagnosticos: [],
-          observaciones: '',
-        }
-        console.log(
-          'El usuario confirmó la cancelación y los campos fueron reseteados'
-        )
-      } else {
-        console.log('El usuario decidió no cancelar')
-      }
-    },
     validarFormulario() {
-      if (this.nuevoPaciente.nombres === '') {
-        alert('El campo nombres es obligatorio')
+      if (this.nuevoPaciente.nombres.trim() === '') {
+        this.mensajeError = 'El campo Nombres es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.apellido1 === '') {
-        alert('El campo primer apellido es obligatorio')
+      } else if (this.nuevoPaciente.apellido1.trim() === '') {
+        this.mensajeError = 'El campo Primer apellido es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.apellido2 === '') {
-        alert('El campo segundo apellido es obligatorio')
+      } else if (this.nuevoPaciente.apellido2.trim() === '') {
+        this.mensajeError = 'El campo Segundo apellido es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.rut === '') {
-        alert('El campo rut es obligatorio')
+      } else if (this.nuevoPaciente.rut.trim() === '') {
+        this.mensajeError = 'El campo RUT es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.sexo === '') {
-        alert('El campo sexo es obligatorio')
+      } else if (this.nuevoPaciente.rut.lenght < 11) {
+        this.mensajeError = 'El campo RUT debe tener al menos 11 caracteres'
         return false
-      }
-      if (this.nuevoPaciente.fechaNacimiento === '') {
-        alert('El campo fecha de nacimiento es obligatorio')
+      } else if (this.nuevoPaciente.sexo === '') {
+        this.mensajeError = 'Debe seleccionar el sexo del paciente'
         return false
-      }
-      if (this.nuevoPaciente.telefono === '') {
-        alert('El campo teléfono es obligatorio')
+      } else if (this.nuevoPaciente.fechaNacimiento === '') {
+        this.mensajeError = 'El campo Fecha de nacimiento es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.direccion === '') {
-        alert('El campo dirección es obligatorio')
+      } else if (this.nuevoPaciente.telefono.trim() === '') {
+        this.mensajeError = 'El campo Teléfono es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.riesgo === '') {
-        alert('El campo riesgo es obligatorio')
+      } else if (this.nuevoPaciente.direccion.trim() === '') {
+        this.mensajeError = 'El campo Dirección es obligatorio'
         return false
-      }
-      if (this.nuevoPaciente.diagnosticos.length === 0) {
-        alert('Debe seleccionar al menos un diagnóstico')
+      } else if (this.nuevoPaciente.riesgo === '') {
+        this.mensajeError = 'Debe seleccionar un riesgo'
+        return false
+      } else if (this.nuevoPaciente.diagnosticos.length === 0) {
+        this.mensajeError = 'Debe seleccionar al menos un diagnóstico'
         return false
       }
       return true
-    },
-    mapSexoANum(sexo) {
-      switch (sexo) {
-        case 'Masculino':
-          return 1
-        case 'Femenino':
-          return 2
-        case 'No aplicable':
-          return 9
-        default:
-          return 0
-      }
-    },
-    mapRiesgoANum(riesgo) {
-      switch (riesgo) {
-        case 'Bajo':
-          return 1
-        case 'Medio':
-          return 2
-        case 'Alto':
-          return 3
-        default:
-          return 0
-      }
     },
     formatoNombre(campo) {
       if (campo === 'nombres') {
@@ -421,28 +377,44 @@ export default {
       )
     },
     formatoRut() {
-      // Eliminar caracteres no numéricos en el cuerpo del RUT, excepto el último dígito después del guion
-      let cleanedRut = this.nuevoPaciente.rut.replace(/[^0-9kK]/g, '')
+      this.nuevoPaciente.rut = this.nuevoPaciente.rut.replace(/[^0-9kK]/g, '')
+      let rut = this.nuevoPaciente.rut
 
-      // Separar el RUT en dos partes: cuerpo y dígito verificador
-      let rutBody = cleanedRut.slice(0, -1)
-      let rutVerifier = cleanedRut.slice(-1).toLowerCase() // Convertir a minúscula
+      // Formatear el RUT
+      if (rut.length > 7) {
+        // Obtener parte numérica y dígito verificador
+        const parteNumerica = rut.slice(0, -1)
+        const digitoVerificador = rut.slice(-1).toLowerCase()
 
-      // Aplicar formato al cuerpo del RUT permitiendo solo números
-      if (rutBody.length <= 8) {
-        this.nuevoPaciente.rut = rutBody.replace(
-          /^(\d{0,2})(\d{0,3})(\d{0,3})/,
-          (_, p1, p2, p3) => {
-            let formatted = p1 ? p1 + '.' : ''
-            formatted += p2 ? p2 + '.' : ''
-            formatted += p3 ? p3 + '-' : ''
-            return formatted
-          }
+        // Formatear la parte numérica
+        let rutFormateado = parteNumerica.replace(
+          /(\d)(?=(\d{3})+(?!\d))/g,
+          '$1.'
         )
-      }
 
-      // Agregar el dígito verificador
-      this.nuevoPaciente.rut += rutVerifier
+        // Agregar el guión y el dígito verificador
+        rutFormateado = rutFormateado + '-' + digitoVerificador
+
+        // Actualizar el valor del RUT en el modelo
+        this.nuevoPaciente.rut = rutFormateado
+      }
+    },
+    limpiarFormulario() {
+      this.nuevoPaciente = {
+        rut: '',
+        nombres: '',
+        apellido1: '',
+        apellido2: '',
+        fecha_nacimiento: null,
+        sexo: '',
+        telefono: '',
+        direccion: '',
+        riesgo: '',
+        diagnosticos: [],
+        gestor: 1,
+      }
+      this.mensajeError = ''
+      this.mensajeAviso = ''
     },
   },
 }
